379090ceaa8309c5b675b705646f9ee422a32ccb
AA <aljo53791@edu.ucl.dk>
2023-11-14 03:54:37 +0100

    added unit test

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,48 @@
+using DogKennel.Model;
+using DogKennel.ViewModels;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ViewModel _viewModel;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _viewModel = new ViewModel();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //ARRANGE
+
+            //ACT
+
+
+            //ASSERT
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+        }
+
+        [TestMethod]
+        public void DataBaseSelectAll()
+        {
+            //ARRANGE
+            
+            //ACT
+
+            //ASSERT
+        }
+    }
+}
